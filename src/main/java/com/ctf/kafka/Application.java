42acb39341149316b865aa19eae7a9694cf81a8b
Chris Flaherty <chris@chrisflaherty.net>
2020-02-28 14:38:59 +1000

    Imports tidy up

--- src/main/java/com/ctf/kafka/Application.java
+++ src/main/java/com/ctf/kafka/Application.java
@@ -1,9 +1,7 @@
 package com.ctf.kafka;
 
-import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
