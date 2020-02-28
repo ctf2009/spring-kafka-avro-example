9aa0e9d95e5d06cdabdb9613dd890e12730fc46f
Chris Flaherty <chris@chrisflaherty.net>
2020-02-28 16:32:50 +1000

    Added second Integration Test

--- src/test/java/com/ctf/kafka/KafkaAvroIntegrationTest.java
+++ src/test/java/com/ctf/kafka/KafkaAvroIntegrationTest.java
@@ -1,7 +1,7 @@
 package com.ctf.kafka;
 
-import com.ctf.kafka.store.MessageStore;
 import com.ctf.kafka.service.ProducerService;
+import com.ctf.kafka.store.MessageStore;
 import org.awaitility.Awaitility;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -15,11 +15,12 @@ import org.springframework.test.annotation.DirtiesContext;
 import java.time.Duration;
 
 import static java.time.temporal.ChronoUnit.SECONDS;
+import static org.springframework.test.annotation.DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD;
 
 @SpringBootTest
 @EmbeddedKafka
 @ExtendWith(MockitoExtension.class)
-@DirtiesContext
+@DirtiesContext(classMode = BEFORE_EACH_TEST_METHOD)
 class KafkaAvroIntegrationTest {
 
     private static final String DEFAULT_MESSAGE_CONTENT = "This is a message";
@@ -32,9 +33,17 @@ class KafkaAvroIntegrationTest {
 
     @Test
     @DisplayName("A message is sent, consumed and stored successfully")
-    public void sentMessageIsSuccessfullyProcessed() {
+    public void sentMessageIsSuccessfullyProcessed() throws Exception {
         givenMessageIsProduced(DEFAULT_MESSAGE_CONTENT);
         thenMessageStoreShouldHaveCount(1);
+    }
+
+    @Test
+    @DisplayName("Multiple messages are sent, consumed and stored successfully")
+    public void multipleSentMessageIsSuccessfullyProcessed() {
+        givenMessageIsProduced(DEFAULT_MESSAGE_CONTENT);
+        givenMessageIsProduced(DEFAULT_MESSAGE_CONTENT);
+        thenMessageStoreShouldHaveCount(2);
     }
 
     private void givenMessageIsProduced(final String messageContent) {
@@ -42,7 +51,7 @@ class KafkaAvroIntegrationTest {
     }
 
     private void thenMessageStoreShouldHaveCount(int messageCount) {
-        Awaitility.await().atMost(Duration.of(5, SECONDS)).until(() ->
+        Awaitility.await().atMost(Duration.of(10, SECONDS)).until(() ->
                 this.messageStore.getMessageCount() == messageCount);
     }
 }
