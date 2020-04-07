c68130f748eb5387fd7e63893720df1a15d3610d
Chris Flaherty <chris@chrisflaherty.net>
2020-04-07 10:41:45 +1000

    Added Pause / Resume 2

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # spring-kafka-avro-example
 A simple demo of Spring Boot integration with Kafka / Schema Repo / Avro
+
+When running with the docker compose, you can run commands like the following to obtain information from kafka
+
+```
+docker run --net=host confluentinc/cp-server:5.4.0 /usr/bin/kafka-consumer-groups --list --bootstrap-server localhost:9092`
+
+```
+
+```
+docker run --net=host confluentinc/cp-server:5.4.0 /usr/bin/kafka-consumer-groups --group testing-group --describe  --bootstrap-server localhost:9092
+```
+
+```
+docker run --net=host confluentinc/cp-server:5.4.0 /usr/bin/kafka-topics --list --bootstrap-server localhost:9092
+```
+
+```
+docker run --net=host confluentinc/cp-server:5.4.0 /usr/bin/kafka-topics --topic test-topic --describe  --bootstrap-server localhost:9092
+```
+
--- src/main/java/com/ctf/kafka/controller/ConsumerController.java
+++ src/main/java/com/ctf/kafka/controller/ConsumerController.java
@@ -1,4 +1,51 @@
 package com.ctf.kafka.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.kafka.config.KafkaListenerEndpointRegistry;
+import org.springframework.kafka.listener.MessageListenerContainer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping(value = "/consumers")
 public class ConsumerController {
+
+    @Autowired
+    private KafkaListenerEndpointRegistry registry;
+
+    @RequestMapping(path = "/pause",
+            method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> consumersPause() {
+        this.registry.getAllListenerContainers().forEach(MessageListenerContainer::pause);
+        return new ResponseEntity<>("Accepted", HttpStatus.ACCEPTED);
+    }
+
+    @RequestMapping(path = "/pause/status",
+            method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Map<String, String>> consumersPauseStatus() {
+        final Map<String, String> statusMap =
+                this.registry.getAllListenerContainers().stream()
+                        .collect(Collectors.toMap(
+                                MessageListenerContainer::getListenerId,
+                                m -> m.isContainerPaused() ? "paused" : "running"));
+        return ResponseEntity.ok(statusMap);
+    }
+
+    @RequestMapping(path = "/resume",
+            method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> consumerResume() {
+        this.registry.getAllListenerContainers().forEach(MessageListenerContainer::resume);
+        return new ResponseEntity<>("Accepted", HttpStatus.ACCEPTED);
+    }
+
 }
