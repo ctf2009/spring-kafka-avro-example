031b0c2e35e55cf26c2c2da2527cdf4d9b1cb383
Chris Flaherty <chris@chrisflaherty.net>
2022-01-17 21:01:11 +1000

    Java 17, Spring Boot 2.6.2)

--- src/main/java/com/ctf/kafka/config/KafkaListenerConfiguration.java
+++ src/main/java/com/ctf/kafka/config/KafkaListenerConfiguration.java
@@ -10,8 +10,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
-import org.springframework.kafka.listener.ErrorHandler;
-import org.springframework.kafka.listener.SeekToCurrentErrorHandler;
+import org.springframework.kafka.listener.CommonErrorHandler;
+import org.springframework.kafka.listener.DefaultErrorHandler;
 import org.springframework.util.backoff.FixedBackOff;
 
 import java.util.HashMap;
@@ -23,38 +23,33 @@ public class KafkaListenerConfiguration {
 
     private static final Logger LOG = LoggerFactory.getLogger(KafkaListenerConfiguration.class);
 
-    @Value("${retry.consumer.backoff-ms:1000}")
-    private int backoffMillis;
-
-    @Value("${retry.consumer.max-attempts:10}")
-    private int retryCount;
-
     @Bean
     public ConcurrentKafkaListenerContainerFactory<Object, Object> kafkaListenerContainerFactory(
             ConcurrentKafkaListenerContainerFactoryConfigurer factoryConfigurer,
             ConsumerFactory<Object, Object> kafkaConsumerFactory,
-            ErrorHandler errorHandler) {
+            CommonErrorHandler errorHandler) {
 
         ConcurrentKafkaListenerContainerFactory<Object, Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
         factoryConfigurer.configure(factory, kafkaConsumerFactory);
 
         // No Longer Need to use RetryTemplate & Recovery Callback
-        factory.setErrorHandler(errorHandler);
+        factory.setCommonErrorHandler(errorHandler);
         return factory;
     }
 
     @Bean
-    public ErrorHandler errorHandler() {
-        final var seekToCurrentErrorHandler = new SeekToCurrentErrorHandler((consumerRecord, exception) ->
-                        LOG.warn("Handing Error for consumerRecord {} with exception {}", consumerRecord, exception.getClass()),
-                        // Do any further processing here, Once complete, the recovered Error is committed
-                        new FixedBackOff(backoffMillis, retryCount));
+    public CommonErrorHandler errorHandler(@Value("${retry.consumer.backoff-ms:1000}") final int backoffMillis,
+                                           @Value("${retry.consumer.max-attempts:10}") final int retryCount) {
+        final var errorHandler = new DefaultErrorHandler((consumerRecord, exception) ->
+                LOG.warn("Handing Error for consumerRecord {} with exception {}", consumerRecord, exception.getClass()),
+                // Do any further processing here, Once complete, the recovered Error is committed
+                new FixedBackOff(backoffMillis, retryCount));
 
-        seekToCurrentErrorHandler.setClassifications(exceptionClassifications(), false);
+        errorHandler.setClassifications(exceptionClassifications(), false);
 
         // This is required to be set to true so that failures that are handled are committed
-        seekToCurrentErrorHandler.setCommitRecovered(true);
-        return seekToCurrentErrorHandler;
+        errorHandler.setCommitRecovered(true);
+        return errorHandler;
     }
 
     private Map<Class<? extends Throwable>, Boolean> exceptionClassifications() {
--- src/test/java/com/ctf/kafka/KafkaAvroIntegrationTest.java
+++ src/test/java/com/ctf/kafka/KafkaAvroIntegrationTest.java
@@ -1,26 +1,27 @@
 package com.ctf.kafka;
 
+import com.ctf.kafka.processor.MessageProcessor;
 import com.ctf.kafka.service.ProducerService;
 import com.ctf.kafka.store.MessageStore;
+import ctf.avro.Error;
 import org.awaitility.Awaitility;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.kafka.test.context.EmbeddedKafka;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import java.time.Duration;
 
 import static java.time.temporal.ChronoUnit.SECONDS;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.annotation.DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 @ActiveProfiles("integration")
 @SpringBootTest
@@ -37,9 +38,12 @@ class KafkaAvroIntegrationTest {
     @Autowired
     private MessageStore messageStore;
 
+    @SpyBean
+    private MessageProcessor messageProcessor;
+
     @Test
-    @DisplayName("A message is sent, consumed and stored successfully")
-    void sentMessageIsSuccessfullyProcessed() throws Exception {
+    @DisplayName("Message consumed and stored successfully")
+    void messageSentAndStoredSuccessfully() {
         givenMessageIsProduced(DEFAULT_MESSAGE_CONTENT);
         thenMessageStoreShouldHaveCount(1);
     }
@@ -49,15 +53,50 @@ class KafkaAvroIntegrationTest {
     void multipleSentMessageIsSuccessfullyProcessed() {
         givenMessageIsProduced(DEFAULT_MESSAGE_CONTENT);
         givenMessageIsProduced(DEFAULT_MESSAGE_CONTENT);
-        thenMessageStoreShouldHaveCount(2);
+        givenMessageIsProduced(DEFAULT_MESSAGE_CONTENT);
+        thenMessageStoreShouldHaveCount(3);
     }
+
+    @Test
+    @DisplayName("RecoverableException is retried correct number of times")
+    void recoverableExceptionIsRetriedCorrectNumberOfTimes() {
+        givenMessageIsProduced(DEFAULT_MESSAGE_CONTENT, Error.RECOVERABLE);
+
+        // 4 = Initial Attempt + 3 Retries
+        verify(messageProcessor, timeout(10000).times(4)).processMessage(any());
+    }
+
+    @Test
+    @DisplayName("UnrecoverableException is never retried")
+    void unrecoverableExceptionIsNeverRetried() {
+        givenMessageIsProduced(DEFAULT_MESSAGE_CONTENT, Error.UNRECOVERABLE);
+
+        // 1 = Initial Attempt + 0 Retries
+        verify(messageProcessor, after(10000).times(1)).processMessage(any());
+    }
+
+    @Test
+    @DisplayName("Nested RecoverableException is retried correct number of times")
+    void nestedRecoverableExceptionIsRetried() {
+        givenMessageIsProduced(DEFAULT_MESSAGE_CONTENT, Error.NESTED_RECOVERABLE);
+
+        // 4 = Initial Attempt + 3 Retries
+        verify(messageProcessor, timeout(10000).times(4)).processMessage(any());
+    }
+
 
     private void givenMessageIsProduced(final String messageContent) {
         this.producerService.process(messageContent, null);
     }
 
+    private void givenMessageIsProduced(final String messageContent, Error errorToThrow) {
+        this.producerService.process(messageContent, errorToThrow);
+    }
+
     private void thenMessageStoreShouldHaveCount(int messageCount) {
-        Awaitility.await().atMost(Duration.of(10, SECONDS)).until(() ->
-                this.messageStore.count() == messageCount);
+        Awaitility.await()
+                .atMost(Duration.of(10, SECONDS))
+                .pollInterval(Duration.of(2, SECONDS))
+                .until(() -> this.messageStore.count() == messageCount);
     }
 }
